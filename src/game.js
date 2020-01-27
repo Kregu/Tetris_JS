a796486804a4fd8d39a9b93ade434da8c5a5cce3
Sergey Gorokhov <sergey.login@gmail.com>
2020-01-28 01:27:41 +0300

    Rotate blocks variant 1 (fix calculate rotate collision)

--- src/game.js
+++ src/game.js
@@ -80,9 +80,8 @@ export default class Game {
 
     rotatePiece() {
         this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0;
-        console.log(this.hasCollision())
         if (this.hasCollision()) {
-            this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3;
+            this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3;
         }
         return this.activePiece.blocks;
     }
