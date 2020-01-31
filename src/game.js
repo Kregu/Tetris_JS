7242a5f4fbaaae261823c9523f2647c4b404452e
Sergey Gorokhov <sergey.login@gmail.com>
2020-02-01 00:41:13 +0300

    Add more blocks

--- src/game.js
+++ src/game.js
@@ -4,16 +4,9 @@ export default class Game {
     lines = 0;
     level = 0;
     playfield = this.createPlayfield();
-    activePiece = {
-        x: 0,
-        y: 0,
-        blocks: [
-            [0,1,0],
-            [1,1,1],
-            [0,0,0],
-        ]
-    };
-
+    activePiece = this.createPiece();
+    nextPiece = this.createPiece();
+    
     getState() {
         const playfield = this.createPlayfield();
         const { y: pieceY, x: pieceX, blocks } = this.activePiece;
@@ -37,7 +30,7 @@ export default class Game {
 
         return {
             playfield
-        }
+        };
     }
 
     createPlayfield() {
@@ -50,6 +43,79 @@ export default class Game {
             }
         }
         return playfield;
+    }
+
+    createPiece() {
+        const index = Math.floor(Math.random() * 7);
+        const type = 'IJLOSTZ'[index];
+        const piece = {};
+
+        switch (type) {
+            case 'I':
+                piece.blocks = [
+                    [0,0,0,0],
+                    [1,1,1,1],
+                    [0,0,0,0],
+                    [0,0,0,0]
+                ];
+                break;
+
+            case 'J':
+                piece.blocks = [
+                    [0,0,0],
+                    [2,2,2],
+                    [0,0,2]
+                ];
+                break;
+
+            case 'L':
+                piece.blocks = [
+                    [0,0,0],
+                    [3,3,3],
+                    [3,0,0]
+                ];
+                break;
+
+            case 'O':
+                piece.blocks = [
+                    [0,0,0,0],
+                    [0,4,4,0],
+                    [0,4,4,0],
+                    [0,0,0,0]
+                ];
+                break;
+
+            case 'S':
+                piece.blocks = [
+                    [0,0,0],
+                    [0,5,5],
+                    [5,5,0]
+                ];
+                break;
+
+            case 'T':
+                piece.blocks = [
+                    [0,0,0],
+                    [6,6,6],
+                    [0,6,0]
+                ];
+                break;
+
+            case 'Z':
+                piece.blocks = [
+                    [0,0,0],
+                    [7,7,0],
+                    [0,7,7]
+                ];
+                break;
+            default:
+                throw new Error ('Unknown type of blocks');
+        }
+
+        piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+        piece.y = -1;
+
+        return piece;
     }
 
     movePieceLeft() {
@@ -71,6 +137,7 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+            this.updatePieces();
         }
     }
 
@@ -114,9 +181,11 @@ export default class Game {
 
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
-                if (blocks[y][x] &&
-                ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) || this.playfield[pieceY + y][pieceX + x]))
-                {
+                if (
+                    blocks[y][x] &&
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                    this.playfield[pieceY + y][pieceX + x])
+                ) {
                     return true;
                 }              
             }         
@@ -130,9 +199,15 @@ export default class Game {
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
                 if (blocks[y][x]) {
-                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];}
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
             }            
         }
+    }
+
+    updatePieces(){
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece;
     }
 
 }
