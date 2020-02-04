4263867719ec9d0b2a4137d52cad9961b1379e3a
Sergey Gorokhov <sergey.login@gmail.com>
2020-02-05 02:17:55 +0300

    Add end game, pause game and restart game

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 import Game from "./src/game.js";
 import View from "./src/view.js";
+import Controller from "./src/controller.js";
 
 const root = document.querySelector('#root');
 
 const game = new Game();
 const view = new View(root, 480, 640, 20, 10);
+const controller = new Controller(game, view);
 
 window.game = game;
 window.view = view;
+window.controller = controller;
 
-document.addEventListener('keydown', event => {
-    switch(event.keyCode){
-        case 37: // LEFT Arrow
-            game.movePieceLeft();
-            view.renderMainScreen(game.getState());
-            break;
-        case 38: // UP Arrow
-            game.rotatePiece();
-            view.renderMainScreen(game.getState());
-            break;
-        case 39: // RIGHT Arrow
-            game.movePieceRight();
-            view.renderMainScreen(game.getState());
-            break;
-        case 40: // DOWN Arrow
-            game.movePieceDown();
-            view.renderMainScreen(game.getState());
-            break;
-    }
-});
-
-view.renderMainScreen(game.getState());
